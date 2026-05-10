1bc3b51cc05573893a812754ac30fdbc1b48fab2
Ridham Taneja <ridham06042003@gmail.com>
2026-05-10 22:00:56 +0530

    Implemented JWT authentication system

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,71 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
 
-from database import engine
-from models import Base
+from database import SessionLocal, engine
+from models import Base, User
+from schemas import UserCreate, UserLogin
+from auth import hash_password, verify_password, create_access_token
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
 def home():
     return {"message": "TaskFlow Backend Running"}
+
+
+@app.post("/register")
+def register(user: UserCreate, db: Session = Depends(get_db)):
+
+    existing_user = db.query(User).filter(User.email == user.email).first()
+
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    hashed_pw = hash_password(user.password)
+
+    new_user = User(
+        name=user.name,
+        email=user.email,
+        password=hashed_pw
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {"message": "User registered successfully"}
+
+
+@app.post("/login")
+def login(user: UserLogin, db: Session = Depends(get_db)):
+
+    db_user = db.query(User).filter(User.email == user.email).first()
+
+    if not db_user:
+        raise HTTPException(status_code=400, detail="Invalid email")
+
+    if not verify_password(user.password, db_user.password):
+        raise HTTPException(status_code=400, detail="Invalid password")
+
+    token = create_access_token(
+        data={
+            "user_id": db_user.id,
+            "role": db_user.role
+        }
+    )
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
