3a79fd433795ef73db4e3758f4255ed11e3f15bb
Ridham Taneja <ridham06042003@gmail.com>
2026-05-11 11:47:09 +0530

    Final full-stack integration

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from database import SessionLocal, engine
@@ -20,6 +21,14 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 security = HTTPBearer()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def get_db():
     db = SessionLocal()
